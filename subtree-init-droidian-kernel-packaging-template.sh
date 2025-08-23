87a78e78011f5f7b7da26eba35fe405496c40f5d
berbascum <berbascum@ticv.cat>
2025-08-24 00:09:11 +0200

    (subtree-init-dkpt) script: Add initial script
    The introduced script is designed to initialize a git branch
    containing a droidian kernel packaging template,
    in a Linux kernel source dir, as subtree.
    
    This adds the required stuff to configure the kernel and package it for Droidian.
    
    Template's testing repoitory:
    https://github.com/berbascum/droidian-kernel-packaging-templates
    
    The flag --template-branch=<branch-name> is required as script argument.
    
    ADVICE: Some of the functionss used are not present in this script.
    
    This script is automatically called by the main configurator:
    https://github.com/berbascum/droidian-kernel-build-helper-scripts/blob/droidian/configure-kernel-source-for-droidian.sh
    
    Currently is not designed to be executed manually as parent script.
    
    Signed-off-by: berbascum <berbascum@ticv.cat>

---
+++ subtree-init-droidian-kernel-packaging-template.sh
@@ -0,0 +1,113 @@
+#!/bin/bash
+
+## Script to add droidian kernel packaging templates as git subtree
+#
+# Version 0.0.1
+#
+# Upstream-Name: droidian-kernel-build-helper-scripts
+# Source: https://github.com/berbascum/droidian-kernel-build-helper-scripts/tree/subtree-init/droidian-kernel-packaging-templates
+#
+# Copyright (C) 2025 Berbascum <berbascum@ticv.cat>
+# All rights reserved.
+#
+# BSD 3-Clause License
+#
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#    * Redistributions of source code must retain the above copyright
+#      notice, this list of conditions and the following disclaimer.
+#    * Redistributions in binary form must reproduce the above copyright
+#      notice, this list of conditions and the following disclaimer in the
+#      documentation and/or other materials provided with the distribution.
+#    * Neither the name of the <organization> nor the
+#      names of its contributors may be used to endorse or promote products
+#      derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
+check_arg() {
+    for arg in $@; do
+        arg_found="$(echo "${arg}" | grep "\-\-${search_arg_str}" | awk -F'=' '{print $2}')"
+        [ -n "${arg_found}" ] && break
+    done
+}
+
+subtree_init_dkpt_set_vars() {
+    ## Git user/organization
+    [ -z "${git_user_repo_dkpt}" ] && \
+        git_user_repo_dkpt="berbascum"
+    ## Dir where subtree will be initiated
+    [ -z "${subtree_dkpt_dir}" ] && \
+        subtree_dkpt_dir="droidian-kernel-packaging"
+    [ -z "${subtree_dkpt_path}" ] && \
+        subtree_dkpt_path="${subtree_dkpt_dir}"
+    ## Repo name vars
+    [ -z "${repo_name_dkpt}" ] && \
+        repo_name_dkpt="droidian-kernel-packaging-templates"
+    ## Branch name vars
+    [ -z "${repo_branch_name_dkpt}" ] && \
+        repo_branch_name_dkpt="" # Arg supplied, skipped
+
+    ## Set the subtree git remote name
+    git_remote_name="origin-Kpackaging"
+    ## Initialize the git args
+    git_args=""
+}
+
+subtree_init_dkpt_exec() {
+    ## Set url vars for ssh/https raw/api
+    git_user="${git_user_repo_dkpt}"
+    repo_name="${repo_name_dkpt}"
+    git_protocols_def
+
+    ## Set the url var
+    repo_url="${repo_url_proto}/${repo_name}"
+    INFO "repo url defined = ${repo_url}"
+
+    ## Check for --template-branch arg
+    search_arg_str='template-branch='
+    check_arg $@
+    branch_arg="${arg_found}"
+    [ -n "${branch_arg}" ] || abort "Missing --${search_arg_str}<name> arg"
+
+    ## Check if the specified branch exists
+    repo_branch="${branch_arg}"
+    #repo_url= Defined above, after git_protocols_def
+    git_branch_exists
+    template_branch="${branch_found}"
+    info  "template_branch = ${template_branch}"
+
+    ## Add the repo as git remote
+    git remote add -f ${git_remote_name} ${repo_url}
+    git subtree add --prefix=${subtree_dkpt_path} ${git_remote_name} ${template_branch} ${git_args} || abort "Subtree download failed!"
+
+    ./${subtree_dkpt_path}/gitignore-kernel-droidian-patcher.sh
+    git add .gitignore
+    git commit -m "(gitignore) droidian: patch Droidian tracking rules"
+
+    ln -sv ${subtree_dkpt_path}/droidian/ droidian
+    ln -sv ${subtree_dkpt_path}/debian/ debian
+
+    mkdir -v ${subtree_dkpt_path}/scripts
+}
+
+## Start subtree initialization if not done yet.
+subtree_init_dkpt_set_vars
+if [ -d "${subtree_dkpt_path}" ]; then
+    INFO "Dir ${subtree_dkpt_path} found. Skipping subtree_init_dkpt..."
+else
+    check_bin_reqs
+    check_bin_reqs
+    subtree_init_dkpt_exec $@
+fi

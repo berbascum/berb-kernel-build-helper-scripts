2b5b95a3d640bd93f8e745f2bebd2cb639c022ea
berbascum <berbascum@ticv.cat>
2025-08-24 10:09:22 +0200

    Increase main version strings to 0.2.1
    Signed-off-by: berbascum <berbascum@ticv.cat>

--- configure-kernel-source-for-droidian.sh
+++ configure-kernel-source-for-droidian.sh
@@ -2,7 +2,7 @@
 
 ## This script configures a Linux kernel source for Droidian.
 #
-# Version_0.1.1
+# Version_0.2.1
 #
 # Upstream-Name: droidian-kernel-build-helper-scripts
 # Source: https://github.com/berbascum/droidian-kernel-build-helper-scripts

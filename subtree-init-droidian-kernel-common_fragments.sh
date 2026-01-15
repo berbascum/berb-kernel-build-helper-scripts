a6c7ffcc67040c4331d30ec7afbe1cfde0e63a80
berbascum <berbascum@ticv.cat>
2026-01-15 15:01:37 +0100

    (subtree-init-dkcf): improve variables design
    This commit updates the script to match with the main script
    consts refactor v2.
    
    Signed-off-by: berbascum <berbascum@ticv.cat>

--- subtree-init-droidian-kernel-common_fragments.sh
+++ subtree-init-droidian-kernel-common_fragments.sh
@@ -41,25 +41,35 @@ if [ -z "${subtree_dkcf_path}" ]; then
     exit 1
 fi
 
-
-## Local print functions for standalone mode
-if [ -z "${subtree_dkcf_path}" ]; then
-    error() {
-        echo "ERROR: $*"; exit 1
-    }
-
-    info() {
-        echo "INFO: $*"
-    }
-fi
 subtree_init_dkcf_set_vars() {
-    ## Set constants
-    ## Set git_user
-    [ -n "${git_user_repo_dkcf}" ] && git_user="${git_user_repo_dkcf}" || git_user="droidian-devices"
-    ## Set repo_name
-    [ -n "${repo_name_dkcf}" ] && repo_name="${repo_name_dkcf}" || repo_name="common_fragments"
-    ## Set subtree_dir
-    [ -n "${subtree_dkcf_dir}" ] && subtree_dir="${subtree_dkcf_dir}" || subtree_dir="droidian/common_fragments"
+    ## Set the subtree git remote name
+    git_remote_name="upstream-common_fragments"
+    debug  "${FUNCNAME[0]}: ${subtree_name}: Defined git_remote_name = ${git_remote_name}"
+
+    ## Set url vars for ssh/https raw/api
+    repo_name="${repo_git_name_stuff}"
+    debug  "${FUNCNAME[0]}: ${subtree_name}: Defined repo_name = ${repo_name}"
+    git_args="${git_args_repo_subtree_stuff}"
+    debug  "${FUNCNAME[0]}: ${subtree_name}: Defined git_args = ${git_args}"
+
+    ## Conditional url vars protocols_def
+    if [ "${repo_git_mode_stuff}" == "url" ]; then
+        ## Set git user
+        git_user="${git_user_repo_stuff}"
+        debug  "${FUNCNAME[0]}: ${subtree_name}: Defined git_user = ${git_user}"
+        ## Set repo url
+        git_protocols_def
+        repo_url="${repo_url_proto}/${repo_name}"
+        INFO "repo url defined = ${repo_url}"
+    ## Conditional fs-local vars protocols_def
+    elif [ "${repo_git_mode_stuff}" == "fs-local" ]; then
+        repo_fslocal_path="${repo_git_fslocal_path_subtree}"
+        debug  "${FUNCNAME[0]}: ${subtree_name}: Defined repo_fslocal_path = ${repo_fslocal_path}"
+        git_protocols_def
+        INFO "repo fslocal path defined = ${repo_fslocal_path}"
+        INFO "repo fslocal (proto) defined = ${repo_fslocal_proto}"
+    fi
+
     ## Set subtree_path
     if [ -n "${subtree_dkcf_path}" ]; then
         subtree_path="${subtree_dkcf_path}"
@@ -69,30 +79,6 @@ subtree_init_dkcf_set_vars() {
         ## root dir is a symlink and this script runs as standalone.
         test -L droidian && error "The droidian dir in the kernel source root dir is a symlink. Aborting!"
     fi
-
-
-    ## Set url vars for ssh/https raw/api
-    git_user="${git_user_repo_dkcf}"
-    repo_name="${repo_name_dkcf}"
-    ## Returns repo_fslocal_proto|repo_url_proto
-    if [ "${repo_git_mode_subtree_dkcf}" == "url" ]; then
-        ## Set the url var
-        repo_url="${repo_url_proto}/${repo_name}"
-        # OLD repo_url="https://github.com/${git_user}/${repo_name}"
-        git_protocols_def
-        INFO "repo url defined = ${repo_url}"
-    elif [ "${repo_git_mode_subtree_dkcf}" == "fs-local" ]; then
-        repo_fslocal_path="${repo_git_fslocal_path_subtree_dkcf}"
-        git_protocols_def
-        INFO "repo fslocal path defined = ${repo_fslocal_path}"
-        INFO "repo fslocal (proto) defined = ${repo_fslocal_proto}"
-    fi
-
-    ## Set the subtree git remote name
-    git_remote_name="upstream-common_fragments"
-
-    ## Set git_args
-    [ -n "${git_args_subtree_dload_dkcf}" ] && git_args="${git_args_dkcf}" || git_args="--squash"
 }
 
 subtree_init_dkcf_exec() {
@@ -118,27 +104,28 @@ echo ""
     [ -n "${KERNEL_VERSION}" ] || error "$(basename $0): Missing --kver=<MA-VER>.<mi.ver> arg"
 
     ## Check if the specified branch exists
-    repo_branch="${KERNEL_VERSION}-[android\$|android\-common\$]"
+    repo_branch_name="${KERNEL_VERSION}-android\$|${KERNEL_VERSION}-android-common\$"
     #repo_url= Defined above, after git_protocols_def
+
     git_branch_exists
     repo_branch_name="${branch_found}"
-    info  "repo_branch_name = ${repo_branch_name}"
+    info  "repo_branch_name = \"${repo_branch_name}\""
     # OLD branch_found=$(git ls-remote --heads ${repo_url} | grep -E "${KERNEL_VERSION}-(android|android-common)" | awk -F'/' '{print $NF}')
 
     ## Check if exist a branch for the specified kernel version
-    [ -n "${repo_branch_name}" ] || error "$(basename $0): The specified common_fragments branch \"${repo_branch_name}\" does not exist"
-
-    info  "branch_found = ${repo_branch_name}"
+    [ -n "${branch_found}" ] || error "$(basename $0): The specified common_fragments branch \"${branch_found}\" does not exist"
 
     ## Add the repo as git remote
-    if [ "${repo_git_mode_subtree_dkcf}" == "url" ]; then
+    if [ "${repo_git_mode_stuff}" == "url" ]; then
+        debug  "${FUNCNAME[0]}: ${subtree_name}: Adding remote \"${git_remote_name}\" \"${repo_url}\""
         git remote add -f --no-tags ${git_remote_name} ${repo_url}
-    elif [ "${repo_git_mode_subtree_dkcf}" == "fs-local" ]; then
+    elif [ "${repo_git_mode_stuff}" == "fs-local" ]; then
+        debug  "${FUNCNAME[0]}: ${subtree_name}: Adding remote \"${git_remote_name}\" \"${repo_fslocal_path}\""
         git remote add -f --no-tags ${git_remote_name} ${repo_fslocal_path}
     fi
 
-    debug "Command init subtree: git subtree add --prefix=\"${subtree_dkcf_path}\" \"${git_remote_name}\" \"${repo_branch_name}\" \"${git_args}\" || error "Subtree download failed!""
-    git subtree add --prefix=${subtree_dkcf_path} ${git_remote_name} ${repo_branch_name} ${git_args} || error "Subtree download failed!"
+    debug "Command init subtree: git subtree add --prefix=subtree_dkcf_path=\"${subtree_dkcf_path}\" git_remote_name=\"${git_remote_name}\" \"${branch_found}\" git_args=\"${git_args}\""
+    git subtree add --prefix=${subtree_dkcf_path} ${git_remote_name} ${branch_found} ${git_args} || error "Subtree download failed!"
 }
 
 ## Start subtree initialization if not done yet.
